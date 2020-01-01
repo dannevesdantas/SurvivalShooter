8e70b3bdb9a2c3ce591013f0f4dac77597b2dae0
Danilo Dantas <dannevesdantas@users.noreply.github.com>
2019-12-31 22:28:31 -0300

    Initial Commit

---
+++ Assets/_CompletedAssets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace CompleteProject
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float speed = 6f;            // The speed that the player will move at.
+
+
+        Vector3 movement;                   // The vector to store the direction of the player's movement.
+        Animator anim;                      // Reference to the animator component.
+        Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+#if !MOBILE_INPUT
+        int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+        float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+#endif
+
+        void Awake()
+        {
+#if !MOBILE_INPUT
+            // Create a layer mask for the floor layer.
+            floorMask = LayerMask.GetMask("Floor");
+#endif
+
+            // Set up references.
+            anim = GetComponent<Animator>();
+            playerRigidbody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            // Store the input axes.
+            float h = CrossPlatformInputManager.GetAxis("L_XAxis_1");
+            float v = CrossPlatformInputManager.GetAxis("L_YAxis_1");
+
+            // Move the player around the scene.
+            Move(h, v);
+
+            // Turn the player to face the mouse cursor.
+            Turning();
+
+            // Animate the player.
+            Animating(h, v);
+        }
+
+        void Move(float h, float v)
+        {
+            // Set the movement vector based on the axis input.
+            movement.Set(h, 0f, v);
+
+            // Normalise the movement vector and make it proportional to the speed per second.
+            movement = movement.normalized * speed * Time.deltaTime;
+
+            // Move the player to it's current position plus the movement.
+            playerRigidbody.MovePosition(transform.position + movement);
+        }
+
+
+        void Turning()
+        {
+#if !MOBILE_INPUT
+            Vector3 turnDir = new Vector3(CrossPlatformInputManager.GetAxis("R_XAxis_1"), 0f, CrossPlatformInputManager.GetAxis("R_YAxis_1"));
+
+            if (turnDir != Vector3.zero)
+            {
+                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+                Vector3 playerToMouse = (transform.position + turnDir) - transform.position;
+
+                // Ensure the vector is entirely along the floor plane.
+                playerToMouse.y = 0f;
+
+                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+                Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
+
+                // Set the player's rotation to this new rotation.
+                playerRigidbody.MoveRotation(newRotatation);
+            }
+#else
+
+            Vector3 turnDir = new Vector3(CrossPlatformInputManager.GetAxisRaw("Mouse X") , 0f , CrossPlatformInputManager.GetAxisRaw("Mouse Y"));
+
+            if (turnDir != Vector3.zero)
+            {
+                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+                Vector3 playerToMouse = (transform.position + turnDir) - transform.position;
+
+                // Ensure the vector is entirely along the floor plane.
+                playerToMouse.y = 0f;
+
+                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+                Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
+
+                // Set the player's rotation to this new rotation.
+                playerRigidbody.MoveRotation(newRotatation);
+            }
+#endif
+        }
+
+
+        void Animating(float h, float v)
+        {
+            // Create a boolean that is true if either of the input axes is non-zero.
+            bool walking = h != 0f || v != 0f;
+
+            // Tell the animator whether or not the player is walking.
+            anim.SetBool("IsWalking", walking);
+        }
+    }
+}
